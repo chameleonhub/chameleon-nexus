ff7d6c04282e3d7acbd568ebe1b7f44b11a113ae
Sharful <sharful09@gmail.com>
2026-06-04 13:41:31 +0600

    Update login theme and production session security

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -14,14 +14,12 @@ CSRF_TRUSTED_ORIGINS = env.list("DJANGO_CSRF_TRUSTED_ORIGINS")
 # ------------------------------------------------------------------------------
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
 
-SESSION_ENGINE = 'redis_sessions.session'
-redis_session_url = env.cache_url(
-    'REDIS_SESSION_URL', default='redis://redis_cache:6380/2'
-)
+SESSION_ENGINE = "redis_sessions.session"
+redis_session_url = env.cache_url("REDIS_SESSION_URL")
 SESSION_REDIS = {
-    'url': redis_session_url['LOCATION'],
-    'prefix': env.str('REDIS_SESSION_PREFIX', 'session'),
-    'socket_timeout': env.int('REDIS_SESSION_SOCKET_TIMEOUT', 1),
+    "url": redis_session_url["LOCATION"],
+    "prefix": env.str("REDIS_SESSION_PREFIX", "session"),
+    "socket_timeout": env.int("REDIS_SESSION_SOCKET_TIMEOUT", 1),
 }
 
 CACHES = {
@@ -32,20 +30,20 @@ CACHES = {
     ),
 }
 
-SESSION_COOKIE_NAME = env('DJANGO_SESSION_COOKIE_NAME')
-SESSION_COOKIE_DOMAIN = env('DJANGO_SESSION_COOKIE_DOMAIN')
+SESSION_COOKIE_NAME = env.str("DJANGO_SESSION_COOKIE_NAME")
+SESSION_COOKIE_DOMAIN = env.str("DJANGO_SESSION_COOKIE_DOMAIN")
 
 
 # SECURITY
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
-# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
 # SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
-# SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
-# CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
